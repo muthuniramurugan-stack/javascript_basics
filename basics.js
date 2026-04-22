a041a08193cd88718a0cf24fdb1b81c2fa58873a
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-22 22:16:40 +0530

    added

---
+++ basics.js
@@ -0,0 +1,20 @@
+//  single line command
+/* this is used for multiline command */
+console.log("Hello");// used to print a line
+// Variable declaration
+let bookname; // let keyword is used to declare a variable
+// variable initialization
+bookname = "Harry Potter";
+let bookAuthor = "Pari";
+console.log(bookname);
+console.log(bookname , "\t" , bookAuthor )// \t is used to print two values with some space
+console.log(bookAuthor ,"\n",bookname);// \n used to print two values in two different lines
+
+// we can also able to declare variable using var keyword
+var name = "Muthu"; // also able to change the value which was already initalized 
+name = "Mouni";
+console.log(name);
+// when a variable is declared as const it cannot be modified or changed
+const a = "Sandeep";
+a="siva";
+console.log(a);
