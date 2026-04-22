790bb7597959ffc5837655a3373e46c6dde968c4
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-22 23:03:28 +0530

    added

---
+++ datatype.js
@@ -0,0 +1,37 @@
+// in js number data type is used to store whole number and fraction 
+let a = 10;
+let b = 20.87;
+console.log(typeof a);
+console.log(typeof b); // typeof keyword is used to find the type of data type
+// Number → represents numeric values (integer & decimal)
+let num = 10;
+
+// String → represents text (inside quotes)
+let str = "Hello";
+
+// Boolean → represents true or false
+let isTrue = true;
+
+// Undefined → variable declared but not assigned
+let x;
+
+// Null → intentionally empty value
+let y = null;
+
+// BigInt → represents very large numbers
+let big = 12345678901234567890n;
+
+// Symbol → unique identifier
+let sym = Symbol("id");
+
+// Object → collection of key-value pairs
+let obj = { name: "Muthu", age: 20 };
+
+// Array → list of values
+let arr = [1, 2, 3];
+
+// Function → reusable block of code
+function greet() {}
+
+// String → represents text (sequence of characters inside quotes)
+let str1 = "Hello";
