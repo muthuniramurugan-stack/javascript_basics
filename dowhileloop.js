817cd48af907e0f2614998d1bdeff1ffe6caebdd
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-23 16:41:58 +0530

    added

---
+++ dowhileloop.js
@@ -0,0 +1,14 @@
+// A do...while loop is a control structure that executes a block of code first, and then checks a condition to decide whether to repeat.
+// This means the loop always runs at least one time, no matter what.
+//Syntax
+/*
+do {
+    // code
+} while (condition);
+*/
+let i = 10;
+
+do {
+    console.log(i); // 10
+    i++;
+} while (i < 5);  
