0936cc5b865d95a56000c1a2059dac903d5860b6
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-23 12:54:49 +0530

    added

---
+++ elseif.js
@@ -0,0 +1,11 @@
+// Used to check multiple conditions in sequence
+let mark = 80;
+if(mark>=90 && mark<=100){
+    console.log("A");
+}else if(mark>=70 && mark <90){
+    console.log("B");
+}else if(mark>=50 && mark<70){
+    console.log("C");
+}else{
+    console.log("Fail");
+}
