d89d49fec5f082ef7c6bdede0a46679f6578e2b6
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-23 12:43:07 +0530

    added

---
+++ ifelse.js
@@ -0,0 +1,7 @@
+// The if...else statement in JavaScript is used to execute one block of code when a condition is true, and another when it’s false.
+let age = 19;
+if(age>=18){
+    console.log("You are eligible for vote")  // condition executes if true
+}else{
+    console.log("You are not eligible for voting")   // condition executes if false
+}
