ef45ada17c3749db27c18ef17826ad41a558d5fb
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-23 14:58:37 +0530

    added

---
+++ forloop.js
@@ -0,0 +1,11 @@
+// A for loop is used when the number of iterations is known.
+//All three parts (start, condition, update) are written in one line.
+
+// Syntax
+/*for (initialization; condition; update) {
+    // code
+} */
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
