58c10b622f7283380f8f5a47783a72ecec9ac3ed
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-23 14:35:43 +0530

    added

---
+++ leapyear.js
@@ -0,0 +1,15 @@
+let year = 2000;
+
+if (year % 4 === 0) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            console.log("Leap Year");
+        } else {
+            console.log("Not a leap year");
+        }
+    } else {
+        console.log("Leap Year");
+    }
+} else {
+    console.log("Not a leap year");
+}
