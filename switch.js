b0c442e49b00089cbc6840822d79f0f70b31def7
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-23 16:55:43 +0530

    added

---
+++ switch.js
@@ -0,0 +1,22 @@
+// A switch statement is used to check one value against multiple cases.
+let n =4;
+switch(n){
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    default:
+        console.log("Number greater than 3");
+
+}
