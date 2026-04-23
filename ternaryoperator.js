8bf69693c2c0d3044957c443f5225a61903e78dd
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-23 13:07:55 +0530

    added

--- ternaryoperator.js
+++ ternaryoperator.js
@@ -3,5 +3,5 @@
 condition ? value_if_true : value_if_false;
 */ 
 let age = 18;
-let result = (age>=18)? Adult:Child;
+let result = (age>=18)? 'Adult':(age>12)? 'teenager':'child';
 console.log(result);
