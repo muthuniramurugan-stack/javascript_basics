33df868d71c137f01df9fe2f0c557411da2e909d
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-24 21:31:44 +0530

    added

---
+++ in.js
@@ -0,0 +1,10 @@
+// The in operator is used to check if a property exists inside an object
+// object
+// An object is a collection of key–value pairs
+let user = {
+    name:'muthu',
+    age :23,
+};
+
+console.log('age' in user);  // true
+console.log('dob' in user); // false
