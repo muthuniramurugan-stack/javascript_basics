e85bd6a3f5dd5ee9c377e26562251a086e6924c5
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-04-25 13:43:43 +0530

    added

---
+++ arrays.js
@@ -0,0 +1,31 @@
+// An array is a special variable used to store multiple values in a single variable.
+// this is differ from c and java because we can able to store same type of values in an array in c and java
+// but in js we can able to store different type of data 
+
+// creation of array 
+// metgod 01 Array Literal Using square brackets []
+let arr = [1,2,3,4,5];
+console.log(arr);
+
+// method 02 
+let a = new Array();
+a=[1,2,3,4,5,6,7,8];
+console.log(a);
+
+// method 02
+let b = [];
+b=[2,3,4,5,6,7];
+console.log(b);
+
+// method 04
+let ab = [];
+
+ab[0] = 10;
+ab[1] = 20;
+ab[2] = 30;
+
+console.log(ab);
+
+// to access elements
+let veges = ['Carrot', 'Beetroot' , 'Potato'];
+console.log(veges[1]);
